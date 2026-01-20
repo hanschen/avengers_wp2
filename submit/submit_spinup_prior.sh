8c63165efc5fd9606011ad121f983f5e4e7c96ca
Hans Chen <contact@hanschen.org>
2026-01-20 11:10:08 +0100

    Initial commit

---
+++ submit/submit_spinup_prior.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J spinup_prior
+#SBATCH -t 3:00:00
+#SBATCH -N 1
+#SBATCH --exclusive
+#SBATCH --mail-type=all
+#SBATCH --mail-user=hans.chen@chalmers.se
+
+trace ../spinup_prior.ini
---
+++ submit/submit_truth.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J truth
+#SBATCH -t 10:00:00
+#SBATCH -N 1
+#SBATCH --exclusive
+#SBATCH --mail-type=all
+#SBATCH --mail-user=hans.chen@chalmers.se
+
+trace ../truth.ini
